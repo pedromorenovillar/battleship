6863d7f2d37ef0529a6bcc470e82ecfaea972928
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-18 17:11:49 +0100

    test(ship): add test for ship registering hits

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -9,4 +9,9 @@ describe("Ship", () => {
     const ship = new Ship(3);
     expect(ship.length).toBe(3);
   });
+  it("can register hits", () => {
+    const ship = new Ship(2);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
 });
