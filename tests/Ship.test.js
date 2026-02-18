b8791833732b26b237f509f3009a7ce1b6afe264
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-18 17:58:31 +0100

    refactor(test): simplify test structure

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,21 +1,20 @@
 import Ship from "../src/Ship.js";
 
-// isSunk?
-
 describe("Ship", () => {
+  let ship;
+  beforeEach(()=> {
+    ship = new Ship(2)
+  })
   it("has length", () => {
-    const ship = new Ship(3);
-    expect(ship.length).toBe(3);
+    expect(ship.length).toBe(2);
   });
   it("can register hits", () => {
-    const ship = new Ship(2);
     ship.hit();
     expect(ship.hits).toBe(1);
   });
-  it("knows when it is sunk", ()=> {
-    const ship = new Ship(2);
+  it("knows when it is sunk", () => {
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(true);
-  })
+  });
 });
