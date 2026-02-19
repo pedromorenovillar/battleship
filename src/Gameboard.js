8661d946b259ce1c9c37362cca300bbb8c354dc4
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-19 14:23:31 +0100

    refactor(gameboard): shorten placeShip function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -6,37 +6,28 @@ class Gameboard {
       this.board.push(row);
     }
   }
-  placeShip(ship, x, y, direction = "horizontal") {
-    if (direction === "horizontal") {
-      // Out of bounds validation
-      if (y + ship.length > 10) {
-        throw Error("Cannot place ship outside board.");
+  placeShip(ship, start, end, direction = "horizontal") {
+    // Swap coordinates depending on direction
+    const dStart = direction === "vertical" ? 1 : 0;
+    const dEnd = direction === "horizontal" ? 1 : 0;
+
+    // Bounds check
+    if (start + dStart * (ship.length - 1) >= 10 || end + dEnd * (ship.length - 1) >= 10) {
+      throw Error("Cannot place ship outside board.");
+    }
+    // Validation loop to check if all spots are free
+    for (let i = 0; i < ship.length; i++) {
+      const row = start + dStart * i;
+      const col = end + dEnd * i;
+      if (this.board[row][col] !== null) {
+        throw Error("Cannot place ship where there is already one.");
       }
-      // Validation loop to check if all spots are free
-      for (let i = 0; i < ship.length; i++) {
-        if (this.board[x][y + i] !== null) {
-          throw Error("Cannot place ship where there is already one.");
-        }
-      }
-      // Placement loop
-      for (let i = 0; i < ship.length; i++) {
-        this.board[x][y + i] = ship;
-      }
-    } else if (direction === "vertical") {
-      // Out of bounds validation
-      if (x + ship.length > 10) {
-        throw Error("Cannot place ship outside board.");
-      }
-      // Validation loop to check if all spots are free
-      for (let i = 0; i < ship.length; i++) {
-        if (this.board[x + i][y] !== null) {
-          throw Error("Cannot place ship where there is already one.");
-        }
-      }
-      // Placement loop
-      for (let i = 0; i < ship.length; i++) {
-        this.board[x + i][y] = ship;
-      }
+    }
+    // Placement loop
+    for (let i = 0; i < ship.length; i++) {
+      const row = start + dStart * i;
+      const col = end + dEnd * i;
+      this.board[row][col] = ship;
     }
   }
 }
