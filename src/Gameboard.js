ce9c46eb55c11eaa88fa2f3d663cbe894ab0cff8
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-19 12:31:34 +0100

    feat(gameboard): initialize gameboard with null values

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,11 +1,10 @@
 class Gameboard {
   constructor() {
-    this.board = []
-    for(let i = 0; i<10;i++) {
-      let row = new Array(10)
-      this.board.push(row)
-    } 
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      let row = new Array(10).fill(null);
+      this.board.push(row);
+    }
   }
-
 }
 export default Gameboard;
