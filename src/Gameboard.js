bb2b7bc924af4b109e0e370ef32aaf8e438fce0a
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-19 16:56:08 +0100

    refactor(gameboard): updated argument names in placeShip function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -9,27 +9,27 @@ class Gameboard {
       this.board.push(row);
     }
   }
-  placeShip(ship, start, end, direction = "horizontal") {
+  placeShip(ship, x, y, direction = "horizontal") {
     // Swap coordinates depending on direction
-    const dStart = direction === "vertical" ? 1 : 0;
-    const dEnd = direction === "horizontal" ? 1 : 0;
+    const dx = direction === "vertical" ? 1 : 0;
+    const dy = direction === "horizontal" ? 1 : 0;
 
     // Bounds check
-    if (start + dStart * (ship.length - 1) >= 10 || end + dEnd * (ship.length - 1) >= 10) {
+    if (x + dx * (ship.length - 1) >= 10 || y + dy * (ship.length - 1) >= 10) {
       throw Error("Cannot place ship outside board.");
     }
     // Validation loop to check if all spots are free
     for (let i = 0; i < ship.length; i++) {
-      const row = start + dStart * i;
-      const col = end + dEnd * i;
+      const row = x + dx * i;
+      const col = y + dy * i;
       if (this.board[row][col] !== null) {
         throw Error("Cannot place ship where there is already one.");
       }
     }
     // Placement loop
     for (let i = 0; i < ship.length; i++) {
-      const row = start + dStart * i;
-      const col = end + dEnd * i;
+      const row = x + dx * i;
+      const col = y + dy * i;
       this.board[row][col] = ship;
     }
   }
