9b1aa86dec566e4b29cdbf5fa60a0cdc84249e4d
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-20 16:14:29 +0100

    test(gameplay): verify that currentPlayer can attack

--- tests/Gameplay.test.js
+++ tests/Gameplay.test.js
@@ -1,18 +1,25 @@
 import Game from "../src/Gameplay";
 
-// Confirm players are assigned
-// Confirm turn starts with P1
-
-describe('Gameplay', ()=> {
-  it('starts game with two players', () => {
+describe("Gameplay", () => {
+  it("starts game with two players", () => {
     const game = new Game();
-    expect(game.players.length).toBe(2)
-    expect(game.currentPlayer).toBe(game.players[0])
-  })
-  it('switching turns changes player', ()=> {
-    const game = new Game()
-    game.nextTurn()
+    // Confirm players are assigned
+    expect(game.players.length).toBe(2);
+    // Confirm turn starts with P1
+    expect(game.currentPlayer).toBe(game.players[0]);
+  });
+  it("switching turns changes player", () => {
+    const game = new Game();
+    game.nextTurn();
 
-    expect(game.currentPlayer).toBe(game.players[1])
-  })
-})
+    expect(game.currentPlayer).toBe(game.players[1]);
+  });
+  it("current player can attack opponent", () => {
+    const game = new Game();
+    const opponent = game.players[1]
+
+    game.attackCurrentPlayer(opponent, 0, 0)
+
+    expect(opponent.gameboard.missedAttacks).toContainEqual([0,0]);
+  });
+});
