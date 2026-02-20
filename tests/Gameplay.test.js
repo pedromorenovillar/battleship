768b8c39e2ffbc9f1138f939d48ea427c9d617b8
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-20 15:51:45 +0100

    test(gameplay): fix missing arrow in first test

--- tests/Gameplay.test.js
+++ tests/Gameplay.test.js
@@ -1,11 +1,10 @@
-import Gameplay from "../src/Gameplay";
+import Game from "../src/Gameplay";
 
-// Initialize game with two players
 // Confirm players are assigned
 // Confirm turn starts with P1
 
 describe('Gameplay', ()=> {
-  it('starts game with two players', () {
+  it('starts game with two players', () => {
     const game = new Game();
     expect(game.players.length).toBe(2)
     expect(game.currentPlayer).toBe(game.players[0])
