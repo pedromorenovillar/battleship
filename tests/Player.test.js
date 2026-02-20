a8049626176aefc172ac0f7c186b3f23dde47b9d
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-20 14:02:51 +0100

    test(player): verify if player can attack other player's gameboard

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -1,15 +1,20 @@
-// Player has a gameboard
 // Can attack other player's gameboard
 // Can be marked as CPU player
 import Player from "../src/Player";
 import Gameboard from "../src/Gameboard";
 
-
 describe("Player", () => {
   it("has a gameboard", () => {
-    let P1 = new Player();
-    expect(P1.gameboard).toBeInstanceOf(Gameboard)
+    const player1 = new Player();
+    expect(player1.gameboard).toBeInstanceOf(Gameboard);
   });
-  it("can attack other player's gameboard", () => {});
+  it("can attack other player's gameboard", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+
+    player1.attack(player2, 0, 0);
+
+    expect(player2.gameboard.missedAttacks).toContainEqual([0, 0]);
+  });
   it("can be marked as CPU player", () => {});
 });
