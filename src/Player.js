3930dd4eaa16e42e83444c1f825544212504d339
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-20 14:06:16 +0100

    feat(player): implement attack on opponent gameboard

--- src/Player.js
+++ src/Player.js
@@ -2,9 +2,11 @@ import Gameboard from "./Gameboard";
 
 class Player {
   constructor() {
-    this.gameboard = new Gameboard
+    this.gameboard = new Gameboard();
   }
-
+  attack(opponent, x, y) {
+    opponent.gameboard.receiveAttack(x, y)
+  }
 }
 
 export default Player;
