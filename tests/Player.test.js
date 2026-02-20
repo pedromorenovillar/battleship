b9ec9fefd8633645626b698ff2651488021d51b7
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-20 13:54:32 +0100

    test(player): correct test matcher to check instance of gameboard

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -1,11 +1,14 @@
 // Player has a gameboard
 // Can attack other player's gameboard
 // Can be marked as CPU player
+import Player from "../src/Player";
+import Gameboard from "../src/Gameboard";
+
 
 describe("Player", () => {
   it("has a gameboard", () => {
     let P1 = new Player();
-    expect(P1.gameboard).toBe(true);
+    expect(P1.gameboard).toBeInstanceOf(Gameboard)
   });
   it("can attack other player's gameboard", () => {});
   it("can be marked as CPU player", () => {});
