a304ecdca1b9f581faf69ca4eb3f71e2b7fac1b8
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-25 15:49:50 +0100

    feat(ui): implement gameboard creation

--- public/index.js
+++ public/index.js
@@ -1,7 +1,30 @@
+import Game from "../src/Gameplay.js";
+
 // Create a new Game
+const game = new Game();
 
 // Render boards
+const playerBoardContainer = document.getElementById('player-board')
+const CPUBoardContainer = document.getElementById('cpu-board')
+
+createBoard(playerBoardContainer)
+createBoard(CPUBoardContainer)
 
 // Attach click listeners
 
 // Update DOM after every move
+
+function createBoard(container) {
+  container.innerHTML = "";
+
+  for (let row = 0; row < 10; row ++) {
+    for (let col = 0; col < 10; col++) {
+      const cell = document.createElement("div")
+      cell.dataset.row = row;
+      cell.dataset.col = col;
+      cell.classList.add("cell")
+
+      container.appendChild(cell)
+    }
+  }
+}
