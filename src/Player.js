f8e4f52612864f1f1d965bd0eb1ba3f13b755d00
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-02-26 16:25:07 +0100

    feat(player): include adjacent cell targeting for cpu

--- src/Player.js
+++ src/Player.js
@@ -5,16 +5,16 @@ class Player {
     this.gameboard = new Gameboard();
     this.isCPU = isCPU;
     this.triedMoves = [];
-    this.targetQueue = []
+    this.targetQueue = [];
   }
   attack(opponent, x, y) {
     const result = opponent.gameboard.receiveAttack(x, y);
     if (this.isCPU == true) {
       if (result === "hit") {
-        this.processHit(x, y)
+        this.processHit(x, y);
       }
     }
-    return result
+    return result;
   }
 
   getRandomMove() {
@@ -29,10 +29,41 @@ class Player {
   }
 
   processHit(x, y) {
-    this.targetQueue.push([x-1, y])
-    this.targetQueue.push([x+1, y])
-    this.targetQueue.push([x, y-1])
-    this.targetQueue.push([x, y+1])
+    const candidates = [
+      [x - 1, y],
+      [x + 1, y],
+      [x, y - 1],
+      [x, y + 1],
+    ];
+
+    candidates.forEach(([nx, ny]) => {
+      if (nx >= 0 && nx <= 9 && ny >= 0 && ny <= 9) {
+        // Only queue if not already tried
+        if (!this.triedMoves.some(([r, c]) => r === nx && c === ny)) {
+          this.targetQueue.push([nx, ny]);
+        }
+      }
+    });
+  }
+  getNextMove() {
+    let move;
+
+    if (this.targetQueue.length > 0) {
+      do {
+        move = this.targetQueue.shift();
+      } while (
+        move &&
+        this.triedMoves.some(([r, c]) => r === move[0] && c === move[1])
+      );
+    }
+
+    if (!move) {
+      move = this.getRandomMove();
+    } else {
+      this.triedMoves.push(move);
+    }
+
+    return move;
   }
 }
 
